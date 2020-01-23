4f2b048d8afb4b4204dfc38060215862e4a96824
ff00919 <59099004+ff00919@users.noreply.github.com>
2020-01-23 14:48:26 -0800

    Tutorial 1.2

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the Vehicle Forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
